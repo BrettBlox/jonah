5b8a854256393e9e6b66c94614df388a31d83751
Brett Bloxom <brettbloxom@gmail.com>
2020-03-08 11:43:20 -0600

    Added mdx

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled, { css } from 'styled-components'
-import { NavLink } from './navLink'
+import { NavLink } from './NavLink'
 
 const Header = ({ siteTitle }) => <header>{siteTitle}</header>
 
