636284014b19abf1649d0a425eba9b0b9620c913
Brett Bloxom <brettbloxom@gmail.com>
2020-03-29 21:58:20 -0600

    debugging card radius

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -14,7 +14,7 @@ const Article = styled.article`
   box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3),
     0 -6px 16px -6px rgba(0, 0, 0, 0.025);
   background-color: var(--white);
-  overflow: auto;
+  overflow: hidden;
   text-align: left;
   transition: all 0.15s ease-out;
   height: 100%;
