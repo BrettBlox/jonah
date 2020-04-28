582359617f6364f0baefda13c78e664c12e36544
Brett Bloxom <brettbloxom@gmail.com>
2020-04-28 15:45:45 -0600

    Testing .gitconfig

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const NavLink = styled(Link)`
   color: ${props => (props.color ? props.color : 'var(--green-900)')};
   text-decoration: none;
+  
   font-weight: ${props => (props.fontWeight ? props.fontWeight : 300)};
   text-transform: uppercase;
   font-size: calc(14px + (18 - 14) * ((100vw - 400px) / (1600 - 400)));
