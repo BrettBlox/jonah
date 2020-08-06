a7a9b092c2633eadb3bf311c1db5874b8e0a48b0
Brett Bloxom <brettbloxom@gmail.com>
2020-08-06 09:41:22 -0600

    mobile about image max-width 60vw

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,11 @@ import ReadLink from '../components/read-link'
 const AboutPage = () => {
   const data = useStaticQuery(graphql`
     query AboutQuery {
-      allMarkdownRemark(filter: { fileAbsolutePath: { glob: "**/content/page-content/about.md" } }) {
+      allMarkdownRemark(
+        filter: {
+          fileAbsolutePath: { glob: "**/content/page-content/about.md" }
+        }
+      ) {
         edges {
           node {
             frontmatter {
@@ -37,14 +41,14 @@ const AboutPage = () => {
     <>
       <SEO title='About' image={frontmatter.image.sharp.fluid.src} lang='en' />
       <motion.div initial='exit' animate='enter' exit='exit'>
-      <h1 id={frontmatter.title}>{frontmatter.title}</h1>
+        <h1 id={frontmatter.title}>{frontmatter.title}</h1>
         <motion.div variants={primary}>
           <Image
             css={`
               width: 400px;
               height: 400px;
-              max-width: 70vw;
-              max-height: 70vw;
+              max-width: 60vw;
+              max-height: 60vw;
               border-radius: 50%;
               margin: 0 auto;
               float: left;
@@ -57,7 +61,10 @@ const AboutPage = () => {
             }}
           />
         </motion.div>
-        <motion.section aria-labelledby={frontmatter.title} variants={secondary}>
+        <motion.section
+          aria-labelledby={frontmatter.title}
+          variants={secondary}
+        >
           <div dangerouslySetInnerHTML={{ __html: html }} />
         </motion.section>
         <motion.div variants={secondary}>
